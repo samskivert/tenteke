bf834adff310066c3a3402ee22308e5e6ac11885
Michael Bayne <mdb@samskivert.com>
2019-03-18 10:47:24 -0700

    Skeleton for Ten Teke: drum training webapp.

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as firebase from "firebase/app"
+
+// import * as S from "./stores"
+// import * as A from "./app"
+// import * as UI from "./ui"
+
+// firebase.initializeApp({
+//   apiKey: "AIzaSyDy3Caew0ql16PM0x7laFXTcs6jih_-e8o",
+//   authDomain: "input-output-26476.firebaseapp.com",
+//   projectId: "input-output-26476",
+// })
+
+// const appStore = new S.AppStore()
+ReactDOM.render(
+  <div>Hello world!</div>,
+  document.getElementById("app-root"))
